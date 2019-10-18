25f67620d5e72b09e6f7fa0098550812895c2e33
Greg Hayes <gthelight@gmail.com>
2019-10-18 12:36:25 -0500

    Cards up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Route, Link } from "react-router-dom";
+import CharacterList from "./components/CharacterList.js";
 import Header from "./components/Header.js";
-
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Link to="/">Home</Link>
+      {/* <Link to="/">Characters</Link> */}
+      <CharacterList />
+
+      <Route exact path="/" component={WelcomePage} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard.js";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  // // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    //   // TODO: Add API Request here - must run in `useEffect`
+    //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+
+      .catch(error => {
+        console.log("There was an error returning data", error);
+      });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Characters</h2>
+      {characters.map(char => {
+        return (
+          <CharacterCard
+            key={char.id}
+            name={char.name}
+            status={char.status}
+            species={char.species}
+          />
+        );
+      })}
     </section>
   );
 }
